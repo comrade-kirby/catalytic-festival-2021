401cc6d089971b9a74ec26c44c508f6f48f64145
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2021-08-27 19:35:16 +0200

    move screen styling into new component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import page from 'page'
 	
+	import Screen from './Screen/Screen.svelte'
 	import Home from './Home/Home.svelte'
 	import Input from './Input/Input.svelte'
 	import Location from './Location/Location.svelte'
@@ -35,14 +36,12 @@
 </script>
 
 <main on:keydown={keydown}>
-	<div class='outer-border'>
-		<div class='inner-border'>
-			<div class='content'>
-				<svelte:component this={component} {cityKey} />
-			</div>
-			<Input />
+	<Screen>
+		<div class='content'>
+			<svelte:component this={component} {cityKey} />
 		</div>
-	</div>
+		<Input />
+	</Screen>
 </main>
 
 <style>
@@ -65,45 +64,6 @@
 		background-size: 100px;
 	}
 
-	.outer-border {
-		position: relative;
-		display: flex;
-		flex: 1;
-		padding: 30px 100px;
-		border-radius: 20px;
-		border-top: 15px solid var(--light-olive);
-		border-right: 15px solid var(--light-olive);
-		border-bottom: 15px solid var(--dark-olive);
-		border-left: 15px solid var(--dark-olive);
-	}
-
-	.inner-border {
-		position: relative;
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-		flex: 1;
-		border-top: 10px solid var(--dark-olive);
-		border-right: 10px solid var(--dark-olive);
-		border-bottom: 10px solid var(--light-olive);
-		border-left: 10px solid var(--light-olive);
-		box-shadow:  inset 0px 0px 20px -6px var(--dark-orange);
-		background-color: var(--dark-grey);
-		border-radius: 70px;
-	}
-
-	.inner-border::after {
-		position: absolute;
-		height: 100%;
-		width: 100%;
-		content: "";
-		background-image: url('/images/lines.jpg');
-		filter: sepia(100%) saturate(700%) contrast(2500%) hue-rotate(-50deg);
-		opacity: 0.05;
-		background-size: 80px;
-		border-radius: 70px;
-	}
-
 	.content {
 		z-index: 1;
 		padding: 60px 60px 15px;
@@ -111,42 +71,12 @@
 	}
 
 	@media (max-width: 1100px) {
-		.outer-border {
-			padding: 30px 60px;
-		}
-
 		.content {
 			padding: 30px 30px 5px;
 		}
 	}
 
-	@media (max-width: 800px) {
-		.outer-border {
-			padding: 30px 30px;
-		}
-	}
-
 	@media (max-width: 500px) {
-		.outer-border {
-			padding: 10px;
-			border-top: 5px solid var(--light-olive);
-			border-right: 5px solid var(--light-olive);
-			border-bottom: 5px solid var(--dark-olive);
-			border-left: 5px solid var(--dark-olive);
-		}
-
-		.inner-border {
-			border-top: 5px solid var(--dark-olive);
-			border-right: 5px solid var(--dark-olive);
-			border-bottom: 5px solid var(--light-olive);
-			border-left: 5px solid var(--light-olive);
-			border-radius: 40px;
-		}
-
-		.inner-border::after {
-			border-radius: 40px;
-		}
-
 		.content {
 			padding: 15px 15px 5px;
 		}
---
+++ src/Screen/Screen.svelte
@@ -0,0 +1,84 @@
+<script>
+
+</script>
+
+<div class='frame'>
+  <div class='screen'>
+    <slot></slot>
+  </div>
+</div>
+
+<style>
+  .frame {
+		position: relative;
+		display: flex;
+		flex: 1;
+		padding: 30px 100px;
+		border-radius: 20px;
+		border-top: 15px solid var(--light-olive);
+		border-right: 15px solid var(--light-olive);
+		border-bottom: 15px solid var(--dark-olive);
+		border-left: 15px solid var(--dark-olive);
+	}
+
+  .screen {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		flex: 1;
+		border-top: 10px solid var(--dark-olive);
+		border-right: 10px solid var(--dark-olive);
+		border-bottom: 10px solid var(--light-olive);
+		border-left: 10px solid var(--light-olive);
+		box-shadow:  inset 0px 0px 20px -6px var(--dark-orange);
+		background-color: var(--dark-grey);
+		border-radius: 70px;
+	}
+
+	.screen::after {
+		position: absolute;
+		height: 100%;
+		width: 100%;
+		content: "";
+		background-image: url('/images/lines.jpg');
+		filter: sepia(100%) saturate(700%) contrast(2500%) hue-rotate(-50deg);
+		opacity: 0.05;
+		background-size: 80px;
+		border-radius: 70px;
+	}
+
+  @media (max-width: 1100px) {
+		.frame {
+			padding: 30px 60px;
+		}
+  }
+
+  @media (max-width: 800px) {
+		.frame {
+			padding: 30px 30px;
+		}
+	}
+
+  @media (max-width: 500px) {
+		.frame {
+			padding: 10px;
+			border-top: 5px solid var(--light-olive);
+			border-right: 5px solid var(--light-olive);
+			border-bottom: 5px solid var(--dark-olive);
+			border-left: 5px solid var(--dark-olive);
+		}
+
+		.screen {
+			border-top: 5px solid var(--dark-olive);
+			border-right: 5px solid var(--dark-olive);
+			border-bottom: 5px solid var(--light-olive);
+			border-left: 5px solid var(--light-olive);
+			border-radius: 40px;
+		}
+
+		.screen::after {
+			border-radius: 40px;
+		}
+  }
+</style>
